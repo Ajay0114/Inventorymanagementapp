487b3bc836fb6679398fb3183cf853940f27e146
root <root@NTO-LAP-053.nitcoinc.in>
2024-08-29 12:41:57 +0530

    new cluster

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -19,7 +19,7 @@ data "aws_subnets" "existing_subnets" {
 }
 # IAM role for EKS cluster
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
+  name = "eks-cluster-role-2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -45,7 +45,7 @@ resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller_policy" {
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-eks-cluster"
+  name     = "eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
@@ -60,7 +60,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 }
 
 resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-group-role"
+  name = "eks-node-group-role-2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -91,7 +91,7 @@ resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
 
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "my-eks-node-group"
+  node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = data.aws_subnets.existing_subnets.ids
 
